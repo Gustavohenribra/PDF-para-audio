2bbb3294263444c10cf2012610e9c04b719b0270
Gustavo Henrique <Gustavohenribra@users.noreply.github.com>
2023-07-25 23:12:48 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PDF-to-Audiobook
+Projeto em Python para converter Ebook em pdf para audio
+Inserir o arquivo em PDF para o diretorio e em seguida alterar o nome do file no arquivo main.py
---
+++ main.py
@@ -0,0 +1,21 @@
+import PyPDF3
+import pyttsx3
+import pdfplumber
+
+file = '123.pdf'
+
+book = open(file, 'rb')
+pdfReader = PyPDF3.PdfFileReader(book)
+
+pages = pdfReader.numPages
+
+finalText = ""
+with pdfplumber.open(file) as pdf:
+    for i in range(0, pages): 
+        page = pdf.pages[i]
+        text = page.extract_text()
+        finalText += text
+
+engine = pyttsx3.init()
+engine.save_to_file(finalText, 'audio.mp3')
+engine.runAndWait()
